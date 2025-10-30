54dc76a78c7a708cb982c48fbc6d3487f9b514d7
palakmaithili <115252922+palakmaithili@users.noreply.github.com>
2025-10-30 14:41:48 +0530

    Create merge.py

---
+++ merge.py
@@ -0,0 +1,8 @@
+arr1 = [45, 54, 76,123,25,123] # inputted array 1 of numbers
+
+arr2 = [123, 25, 123, 76, 54, 45] # inputted array 2 of numbers
+
+merged_arrays = arr1 + arr2 # finding the merged array
+
+print(f"The merged array is: {merged_arrays}") # printing the merged array
+
